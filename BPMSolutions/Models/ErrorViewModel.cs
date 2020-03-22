ac113fab327dc3373a236750b4a4fc066146f7b0
Kevin Mendoza López <kmendoza0829@gmail.com>
2020-03-22 18:25:03 -0500

    Ajus

---
+++ BPMSolutions/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BPMSolutions.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
